8ff92d2f6b80e7dca6cc22e41dfe494dbf3bb637
unknown <bryansantos75@gmail.com>
2021-06-27 14:17:54 -0300

    SpringBoot

---
+++ SpringBoot/Hello/src/main/java/com/HelloWord/Hello/HelloApplication.java
@@ -0,0 +1,14 @@
+package com.HelloWord.Hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+@SpringBootApplication
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+
+}
