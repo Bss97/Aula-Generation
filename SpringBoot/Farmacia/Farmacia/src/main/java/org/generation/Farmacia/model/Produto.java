a9ce69fc10de131593f9673023a53b0cbdcc7db6
unknown <bryansantos75@gmail.com>
2021-07-05 23:33:46 -0300

    Farmacia

--- SpringBoot/Farmacia/Farmacia/src/main/java/org/generation/Farmacia/model/Produto.java
+++ SpringBoot/Farmacia/Farmacia/src/main/java/org/generation/Farmacia/model/Produto.java
@@ -1,23 +1,36 @@
 package org.generation.Farmacia.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
-public class Categoria {
+public class Produto {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	@NotNull
 	private String nome;
-	
+
 	@NotNull
 	private String descricao;
+
+	@NotNull
+	private float preco;
+
+	@OneToMany(mappedBy = "produto", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("produto")
+	private List<Categoria> categoria;
 
 	public long getId() {
 		return id;
@@ -42,5 +55,21 @@ public class Categoria {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(List<Categoria> categoria) {
+		this.categoria = categoria;
+	}
+
 }
